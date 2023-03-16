9856a3755e2a7e7ffd89da6c2a71d268889ef8ca
ELkingfelo <ELkingfelo@gmail.com>
2023-03-16 14:27:56 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,9 +6,9 @@
  * Description: A C porgram that with put  function
  *
  * Return: Always 0 (Success)
- */ 
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);	
+	return (0);
 }
